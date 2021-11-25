c425f73d31b5656a88a349fef29d4ac611bd5765
suyeong <86659054+jangsuyeongg@users.noreply.github.com>
2021-11-25 15:45:16 +0900

    CodingTest 2
    CodingTest 2

---
+++ CodingTest/CodingTest/CodingTest2.java
@@ -0,0 +1,20 @@
+
+public class CodingTest2 {
+	public static int[] solution(int[] numbers) {
+        int sums = 0;
+        for(int i=0;i<numbers.length;i++){
+            for(int j=0;j<numbers.length;j++){
+                sums = (numbers[i]+numbers[j]);
+                    
+            }
+        }
+        int[] answer = {};
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		int[] numbers = {0,2,5,7};
+		solution(numbers);
+
+	}
+}
---
+++ CodingTest2/src/CodingTest2.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+
+public class CodingTest2 {
+	public static int[] solution(int[] numbers) {
+		HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<numbers.length;i++){
+            for(int j=i+1;j<numbers.length;j++){
+                set.add(numbers[i]+numbers[j]);
+            }
+        }
+        
+       
+        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+    }
+
+	public static void main(String[] args) {
+		int[] numbers = {0,2,5,7};
+		int[] result = solution(numbers);
+		for(int i=0;i<result.length;i++) {
+			System.out.println(result[i]);
+		}
+
+	}
+}
