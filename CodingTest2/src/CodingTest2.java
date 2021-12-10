d424197762366f7ec37113ba4e54296ec03ee7bc
suyeong <86659054+jangsuyeongg@users.noreply.github.com>
2021-12-10 12:25:20 +0900

    테스트
    테스트

--- CodingTest2/src/CodingTest2.java
+++ CodingTest2/src/CodingTest2.java
@@ -3,13 +3,11 @@ import java.util.HashSet;
 public class CodingTest2 {
 	public static int[] solution(int[] numbers) {
 		HashSet<Integer> set = new HashSet<>();
-        for(int i=0;i<numbers.length;i++){
+        for(int i=0; i<numbers.length; i++){
             for(int j=i+1;j<numbers.length;j++){
                 set.add(numbers[i]+numbers[j]);
             }
         }
-        
-       
         return set.stream().sorted().mapToInt(Integer::intValue).toArray();
     }
 
